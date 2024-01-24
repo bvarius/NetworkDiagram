087d2390314ec1f2703fd1bf1401af8f4c8a7361
bvarius <jacksonheiberger74@gmail.com>
2024-01-24 12:30:34 -0600

    Updated WIndows, particularly software

--- getInfo.py
+++ getInfo.py
@@ -1,11 +1,15 @@
-from os import listdir
 from platform import uname
-from psutil import win_service_iter
 import socket
+
+if (uname().system == 'Windows'):
+    from winreg import ConnectRegistry, HKEY_LOCAL_MACHINE, OpenKey, QueryInfoKey, EnumKey, QueryValueEx
+    import psutil
+else:
+    import apt
 
 def ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("8.8.8.8", 80))
+    s.connect(("138.247.254.1", 80))
     ip = s.getsockname()[0]
     s.close()
     return ip
@@ -14,21 +18,31 @@ def names():
     sys_info = uname()
     return sys_info.node, sys_info.system + ' ' + sys_info.version, sys_info.machine
     
-def services():
+def win_services():
     default = set()
-    services = win_service_iter()
+    services = psutil.win_service_iter()
     service_names = [service.display_name() for service in services if service.start_type() == 'automatic']
     service_names = set(service_names)
     service_names = service_names - default
     return (sorted(service_names))
 
-def software():
-    default = set(['Common Files', 'Internet Explorer', 'MSBuild', 'Reference Assemblies', 'Windows Defender', 'Windows Mail', 'Windows Media Player', 'Windows NT', 'Windows Photo Viewer', 'Windows Portable Devices', 'Windows Sidebar'])
-    programs = listdir(path='C:\Program Files')
-    programs += listdir(path='C:\Program Files (x86)')
-    programs = set(programs)
-    programs = programs - default
-    return sorted(programs)
+def win_software():
+    programs = []
+    reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+    key = OpenKey(reg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
+
+    for i in range(QueryInfoKey(key)[0]):
+        software_key_name = EnumKey(key, i)
+        software_key = OpenKey(key, software_key_name)
+        try:
+            software_version = QueryValueEx(software_key, "DisplayVersion")[0]
+            if (software_key_name[0] != '{' and software_key_name[0]):
+                if (software_version in software_key_name):
+                    software_key_name = software_key_name[:software_key_name.find(software_version)]
+                programs += [software_key_name + ' | ' + software_version]
+        except Exception as e:
+            pass
+    return programs
 
 def write_file(box_name, ip, OS_version, architecture, service_names, programs):
     with open(box_name + "_info.txt", "w") as f:
@@ -47,11 +61,26 @@ def write_file(box_name, ip, OS_version, architecture, service_names, programs):
         for program in programs:
             f.write("\t" + program + "\n")
 
+def lin_software():
+
+    cache = apt.Cache()
+
+    for mypkg in cache:
+        if cache[mypkg.name].is_installed:
+            print(mypkg.name)
+
+def lin_services():
+    return "Linux Bad"
+
 def main():
     box_name, OS_version, architecture = names()
-    programs = software()
-    service_names = services()
     ip = ip_address()
+    if (OS_version.split(' ')[0] == 'Windows'):
+        programs = win_software()
+        service_names = win_services()
+    else:
+        programs = lin_software()
+        service_names = lin_services()
 
     write_file(box_name, ip, OS_version, architecture, service_names, programs)
 
